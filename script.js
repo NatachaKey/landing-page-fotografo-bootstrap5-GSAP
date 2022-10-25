2930fb88f117bec006a6b5f62f7823c24d1651c9
Glitch (glitch-hello-website) <none>
2022-10-25 18:22:17 +0000

    🌺👴 Checkpoint ./script.js:42900803/3 ./style.css:42900803/1615 ./index.html:42900803/458

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-gsap.from("a", {y:-50, delay:0.1, duration:3, opacity:0.5, ease:"power4.out"})
-gsap.from("h2", {x:150, delay:0.1, duration:3, opacity:0 ease:"power4.out"})
-gsap.from("p", {x:150, delay:0.1, duration:3, opacity:0, ease:"power4.out"})
+gsap.from("a", {y:-50, delay:0.1, duration:3, opacity:0.5, ease:"power4.out"});
+gsap.from("h2", {x:150, delay:0.1, duration:3, opacity:0 ease:"power4.out"});
+gsap.from("p", {x:150, delay:0.1, duration:3, opacity:0, ease:"power4.out"});
